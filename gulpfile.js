ac3c68c6c037592eba9a0f40f7d7713414724fc9
PhantasWeng <bensonuong@gmail.com>
2016-03-14 18:38:54 +0800

    Add livereload, http-server, brower-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,82 @@
 var gulp = require('gulp'),
-	concat = require('gulp-concat'),
-  plumber = require('gulp-plumber'),
-	compass = require('gulp-compass'),
-  watch = require('gulp-watch'),
-  batch = require('gulp-batch');
-
+    concat = require('gulp-concat'),
+    // plumber = require('gulp-plumber'),
+    notifier = require('node-notifier'),
+    server = require('http-server'),
+    compass = require('gulp-compass'),
+    watch = require('gulp-watch'),
+    batch = require('gulp-batch'),
+    connect = require('gulp-connect'),
+    livereload = require('gulp-livereload'),
+    browserSync = require('browser-sync').create(),
+    reload = browserSync.reload;
 // 定義路徑
 var path = {
-  'source': './source/',
-  'bower' : './bower_components/',
-  'css': './source/stylesheets/',
-  'img': './source/images/',
-  'public': './public/'
+    'source': './source/',
+    'bower': './bower_components/',
+    'css': './source/stylesheets/',
+    'img': './source/images/',
+    'public': './public/'
 }
+
+// 連結連結 webserver 跟 livereload
+gulp.task('connect', function() {
+    connect.server({
+        root: 'public',
+        livereload: true,
+        port: 8888
+    });
+});
 
 // 合併套件js
 gulp.task('jsLib', function() {
-  gulp.src([
-    path.bower + 'jquery/dist/jquery.min.js',
-    path.bower + 'bootstrap/dist/js/bootstrap.min.js',
-    ])
-    .pipe(plumber())
-    .pipe(concat('vendor.js'))
-    .pipe(gulp.dest( path.public + 'js'))
+    gulp.src([
+            path.bower + 'jquery/dist/jquery.min.js',
+            path.bower + 'bootstrap/dist/js/bootstrap.min.js',
+        ])
+        // .pipe(plumber())
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest(path.public + 'js'))
 });
 
 // 編譯Compass
 gulp.task('compass', function() {
-  return gulp.src(['./source/scss/'])
-  .pipe(plumber())
-  .pipe(compass({
-    config_file: './config.rb' // ** 注意：這裡必需額外加入config.rb **
-  }))
+    return gulp.src(['./source/sass/'])
+        // .pipe(plumber())
+        .pipe(compass({
+            config_file: './config.rb' // ** 注意：這裡必需額外加入config.rb **
+        }))
+        .on('error', function() {
+            notifier.notify({ 'title': 'Gulp', 'message': 'sass Error' });
+        });
+
 });
 
 // 複製 Html 到 Public 資料夾
 gulp.task('html', function() {
-  gulp.src(['./source/**/**.html'])
-  .pipe(plumber())
-  .pipe(gulp.dest( path.public ))
+    gulp.src(['./source/**/**.html'])
+        // .pipe(plumber())
+        .pipe(gulp.dest(path.public))
 });
 
 // 監聽資料夾事件
-gulp.task('watch', function () {
-  watch('./source/scss/**/*.scss', batch(function (events, done) {
-    gulp.start('compass', done);
-  }));
-  watch('./source/**/**.html', batch(function (events, done) {
-    gulp.start('html', done);
-  }));
+gulp.task('watch', function() {
+    watch('./source/sass/**/*.sass', batch(function(events, done) {
+        gulp.start('compass', done);
+    }));
+    watch('./source/**/**.html', batch(function(events, done) {
+        gulp.start('html', done);
+    }));
+    watch(['*.html', './**/*.css', './**/*.js'], { cwd: 'public' }, reload)
 });
 
-gulp.task('default', ['watch','jsLib','compass','html']);
+//瀏覽器重整
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "public"
+        }
+    });
+});
 
-
+gulp.task('default', ['jsLib', 'compass', 'html', 'connect', 'browser-sync', 'watch']);
